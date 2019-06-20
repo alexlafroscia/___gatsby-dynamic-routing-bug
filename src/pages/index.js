5f0b39cf55c578527152ed2b655d47d11869b6bd
Alex LaFroscia <alex@lafroscia.com>
2019-06-20 15:19:15 -0700

    feat: configure dynamic routing from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,15 @@
+import { Router } from "@reach/router"
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const Index = () => <h1>Index Page</h1>
+
+const NotFound = () => <h1>Not Found</h1>
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <Router>
+    <Index path="/" />
+    <NotFound default />
+  </Router>
 )
 
 export default IndexPage
